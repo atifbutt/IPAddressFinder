010003a32a17a0220ddfc08af2a51a0da9afa8a7
Atif Butt <Atif Butt>
2017-10-05 01:44:18 -0700

    Add project files.

---
+++ IPAddressFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IPAddressFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Host = Dns.GetHostEntry(Dns.GetHostName());
+
+            var name = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            var SystemName = name.Split('\\');
+
+            foreach (IPAddress ip in Host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine("User Name  : "+ SystemName[1]);
+                    Console.WriteLine("Host Name  : " + Dns.GetHostEntry(ip).HostName);
+                    Console.WriteLine("IP Address : " + ip);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
